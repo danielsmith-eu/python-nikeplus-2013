3f0d7db8cb39c29e4ad020d9491ac8cb1e9e4fdb
Dr. Daniel A Smith <daniel@danielsmith.eu>
2013-04-18 13:02:30 +0100

    Skeleton of module

---
+++ nikeplus.py
@@ -0,0 +1,24 @@
+#    This file is part of python-nikeplus-2013.
+#
+#    Copyright 2013 Daniel Alexander Smith
+#    Copyright 2013 University of Southampton
+#
+#    python-nikeplus-2013 is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    python-nikeplus-2013 is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with python-nikeplus-2013.  If not, see <http://www.gnu.org/licenses/>.
+
+""" Access and get data from the nikeplus system (using the 2013 API). """
+class NikePlus:
+    
+    def __init__(self, username, password):
+        pass
+
