9fa84a458f028d9b85c2a61d0af1cee71a350607
fredrikaverpil <fredrik.averpil@gmail.com>
2013-08-04 03:03:14 +0200

    Added GPS data retrieval

--- nikeplus.py
+++ nikeplus.py
@@ -138,3 +138,24 @@ class NikePlus:
         return body
 
 
+    def get_gps_data(self, activity_id):
+        """ Get GPS data on a specific activity, based on its ID. """
+
+        url = "https://developer.nike.com/request/"
+
+        body = "data=%7B%22method%22%3A%22GET%22%2C%22url%22%3A%22https%3A%2F%2Fapi.nike.com%2Fme%2Fsport%2Factivities%2F{1}%2Fgps%3Faccess_token%3D{0}%26activityId%3D{1}%22%2C%22headers%22%3A%7B%22appid%22%3A%22%25appid%25%22%2C%22Accept%22%3A%22application%2Fjson%22%7D%2C%22body%22%3A%22%22%2C%22environment%22%3A%22prod%22%7D".format(self.token, activity_id)
+
+        req = urllib2.Request(url, body)
+        req.get_method = lambda: "POST"
+        f = self.opener.open(req)
+        resp = f.read()
+        time.sleep(1)
+        self.logger.debug("get_activities: received resp: {0}".format(pprint.pformat(resp)))
+        
+        response = json.loads(resp)
+        self.logger.debug("get_activities: received response: {0}".format(pprint.pformat(response)))
+
+        body = json.loads(response['body']) # double JSON encoded. seriously.
+        self.logger.debug("get_activities: received response body: {0}".format(pprint.pformat(body)))
+
+        return body
